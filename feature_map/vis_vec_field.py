17b91dec21bd1b455f0522bea0329950c03f0b3d
Sumit Chaturvedi <sumit.chaturvedi@gmail.com>
2024-05-22 18:32:29 +0000

    Added multiple spp for feature maps

--- feature_map/vis_vec_field.py
+++ feature_map/vis_vec_field.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from PIL import Image
 
 # Load the tangent map from the PPM file
-tangent_image = Image.open('tangent_map.ppm')
+tangent_image = Image.open('tangent_map.png')
 tangent_map = np.array(tangent_image) / 255.0 * 2 - 1  # Convert to [-1, 1] range
 
 # Extract the x and y components of the tangent
@@ -14,7 +14,7 @@ V = tangent_map[:, :, 1]
 height, width = tangent_map.shape[:2]
 Y, X = np.mgrid[0:height, 0:width]
 
-density = 64
+density = 60
 
 X = X[::density, ::density]
 Y = Y[::density, ::density]
@@ -25,7 +25,7 @@ V = V[::density, ::density]
 plt.imshow(tangent_image, extent=(0, width, height, 0))
 
 # Overlay the vector field
-plt.quiver(X, Y, U, V, color='red', scale=50)
+plt.quiver(X, Y, U, V, color='red', scale=30)
 
-plt.show()
+plt.savefig('a.png')
 
