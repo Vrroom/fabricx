42f9e077195071b08f96041a712b63ba2ab2669f
mandyxmq <mandyxmq2@gmail.com>
2024-08-07 15:52:34 -0400

    delta transmission

--- spongecake_bsdf.py
+++ spongecake_bsdf.py
@@ -182,173 +182,17 @@ class SurfaceBased (mi.BSDF) :
             cur_dim *= 2
         self.normal_mipmap.append(self.normal_map)
 
-    # def sample(self, ctx, si, sample1, sample2, active) : 
-    #     tiles = self.tiles
-    #     tiled_uv = dr.select(self.feature_map_type == "cloth", (si.uv * tiles) - dr.trunc(si.uv * tiles), si.uv)
-    #     bs = mi.BSDFSample3f() 
-    #     alpha = dr.maximum(0.0001, self.alpha)
-    #     S_surf = dr.diag(mi.Vector3f(alpha * alpha, alpha * alpha, 1.)) 
-    #     S_fibr = dr.diag(mi.Vector3f(1., alpha * alpha, 1.))
-    #     # S_surf = dr.diag(dr.llvm.ad.Array3f(alpha * alpha, alpha * alpha, 1.)) # surface type matrix. Later we'll rotate it and all that.
-    #     # S_fibr = dr.diag(dr.llvm.ad.Array3f(1., alpha * alpha, 1.)) # fiber type matrix. Later we'll rotate it and all that.
-
-    #     normal = mi.Vector3f(self.normal_map.eval(tiled_uv))
-    #     tangent = mi.Vector3f(self.tangent_map.eval(tiled_uv))
-
-    #     delta_transmission = mi.Vector1f(self.delta_transmission_map.eval(tiled_uv))    # TODO: think about the tiled_uv here
-    #     # selected_dt = sample1 > delta_transmission.x
-    #     selected_dt = sample1 > 1.0
-        
-    #     normal = normal / dr.norm(normal)
-    #     tangent= tangent / dr.norm(tangent)
-
-    #     tangent = tangent - dr.sum(tangent * normal) * normal
-
-    #     ####################################################
-    #     ## Micro-scale BSDF
-
-    #     specular_or_diffuse = sample1 < self.specular_prob
-
-    #     ################################
-    #     ## Specular Terms
-
-    #     S = dr.select(self.surface_or_fiber, S_surf, S_fibr)
-    #     S = rotate_s_mat(S, normal, tangent)
-
-    #     h, D, wo, pdf = sample_specular(sample2, si.wi, S)
-    #     h_, D_, wo_, pdf_ = sample_diffuse(self.pcg, sample2, si.wi, S)
-    #     h_dt, D_dt, wo_dt, pdf_dt = mi.Vector3f(0,0,1), 1.0, -si.wi, 1.0
-
-    #     h = dr.select(specular_or_diffuse, h, h_)
-    #     D = dr.select(specular_or_diffuse, D, D_)
-    #     wo = dr.select(specular_or_diffuse, wo, wo_)
-    #     pdf = dr.select(specular_or_diffuse, pdf, pdf_)
-
-    #     # delta transmission
-    #     h = dr.select(selected_dt, h_dt, h)
-    #     D = dr.select(selected_dt, D_dt, D)
-    #     wo = dr.select(selected_dt, wo_dt, wo)
-    #     pdf = dr.select(selected_dt, pdf_dt, pdf)
-
-    #     color = mi.Color3f(self.texture.eval(tiled_uv)) # texture usually uses original uv (si.uv) unless using id map
-    #     F = color + (1.0 - color) * ((1 - dr.abs(dr.dot(h, wo))) ** 5)
-
-    #     cos_theta_i = mi.Frame3f.cos_theta(si.wi) 
-    #     cos_theta_o = mi.Frame3f.cos_theta(wo)
-
-    #     selected_r = (cos_theta_i * cos_theta_o > 0.0)  # reflecting when the two have the same sign
-
-    #     G_r = shadow_masking_term_reflect(si.wi, wo, cos_theta_i, cos_theta_o, self.optical_depth, S)
-    #     G_t = shadow_masking_term_transmit(si.wi, wo, cos_theta_i, cos_theta_o, self.optical_depth, S)
-
-    #     G = dr.select(selected_r, G_r, G_t)
-
-    #     bs.wo = wo
-    #     bs.eta = 1.
-    #     bs.sampled_component = dr.select(selected_dt, 
-    #         mi.UInt32(2), 
-    #         dr.select(selected_r, mi.UInt32(0), mi.UInt32(1)))
-    #     bs.sampled_type = dr.select(selected_dt, 
-    #         mi.UInt32(+mi.BSDFFlags.DeltaTransmission),
-    #         dr.select(selected_r, mi.UInt32(+mi.BSDFFlags.GlossyReflection), mi.UInt32(+mi.BSDFFlags.GlossyTransmission)))
-    #     # bs.pdf = pdf 
-    #     # mandy temporily changes this
-    #     bs.pdf = 1.0
-
-    #     # VISIBILITY
-    #     bent_normal = mi.Vector3f(self.bent_normal_map.eval(tiled_uv))
-    #     asg_params = mi.Vector3f(self.asg_params.eval(tiled_uv))
-    #     di = euclidean_to_spherical_dr(si.wi) 
-    #     do = euclidean_to_spherical_dr(wo) 
-    #     bent_normal = bent_normal / dr.norm(bent_normal)
-    #     mu = euclidean_to_spherical_dr(bent_normal)
-    #     V_i = asg_dr(mu, asg_params.x, asg_params.y, asg_params.z, 1.0, di)
-    #     V_o = asg_dr(mu, asg_params.x, asg_params.y, asg_params.z, 1.0, do)
-
-    #     # v_threshold = 0.5
-    #     active = active & dr.neq(cos_theta_i, 0.0) & dr.neq(D, 0.0) & dr.neq(dr.dot(bs.wo, h), 0.0) # & (V_i > v_threshold) & (V_o > v_threshold)
-    #     # active =  active & (cos_theta_i != 0.0 ) & (D != 0.0) & (dr.dot(bs.wo, h) != 0.0)
-
-    #     f_sponge_cake = (F * D * G) / (4. * dr.abs(cos_theta_i))
-
-    #     ################################
-    #     ## Diffuse Terms
-
-    #     # TODO: check the numerator and denominator, as it is different in the two papers (Jin and SurfaceBased)
-    #     threshold_diffuse = 0.01    # avoid division of near-zero values
-    #     diffuse_sign = dr.select(selected_r, 1.0, -1.0) # negative sign if transmit
-    #     abs_on = abs_dot(diffuse_sign * si.wi, normal)
-    #     abs_in = dr.abs(diffuse_sign * cos_theta_i)
-    #     abs_in = dr.maximum(threshold_diffuse, abs_in)
-    #     f_diffuse = (F / math.pi) * (
-    #         self.w * (abs_on / abs_in) +
-    #         (1.0 - self.w)
-    #     )
-
-    #     f_surface_based_micro = dr.select(specular_or_diffuse, f_sponge_cake, f_diffuse)
-
-    #     ###################################################
-    #     # Meso-scale BSDF
-
-    #     ## TODO: ray differentials: texture space partials (si.duv_dx, si.duv_dy) are [0.0, 0.0]
-    #     # si.bsdf()
-    #     # area = dr.norm(dr.cross(si.duv_dx, si.duv_dy))
-    #     ## mipmap level determination for cylinder
-    #     # mipmap_level = dr.round(dr.sqrt(-dr.log2(area)))
-    #     ## TODO: mipmap level determination for cloth, which should consider tiling
-
-    #     # best threshold value from ASG results for plain type
-    #     v_threshold = 0.15
-    #     V_i = dr.select(V_i <= v_threshold, 0.0, 1.0)
-    #     V_o = dr.select(V_o <= v_threshold, 0.0, 1.0)
-
-    #     cloth_mipmap_dim = 8
-    #     cylinder_mipmap_dim = 256
-    #     mipmap_level = dr.select(self.feature_map_type == "cloth", int(math.log2(cloth_mipmap_dim)), int(math.log2(cylinder_mipmap_dim)))
-
-    #     # threshold_meso = 0.005              # avoid division of near-zero values
-    #     # n_s = mi.Vector3f(0.0, 0.0, 1.0)    # n_s: surface normal
-    #     # # n_f: average normal over pixel footprint
-    #     # # TODO: interpolation between mipmap levels
-    #     # mipmap_level = dr.minimum(mipmap_level, len(self.normal_mipmap)-1)
-    #     # n_f = mi.Vector3f(self.normal_mipmap[mipmap_level].eval(tiled_uv))  # TODO: int(mipmap_level.numpy()[0]) if mipmap_level is not yet int
-    #     # abs_ns_np = abs_dot(n_s, normal)
-    #     # abs_ns_np = dr.maximum(threshold_meso, abs_ns_np)
-    #     # abs_ns_nf = abs_dot(n_s, n_f)
-    #     # abs_ns_nf = dr.maximum(threshold_meso, abs_ns_nf)
-    #     # A_p = (abs_dot(wo, normal)/abs_ns_np) * V_o
-    #     # A_g = abs_dot(wo, n_f)/abs_ns_nf
-    #     # A_g = dr.maximum(threshold_meso, A_g)
-    #     # f_p = f_surface_based_micro * abs_dot(normal, si.wi) * V_i * A_p
-    #     # f_surface_based_meso = f_p / A_g    # k_p and pdf cancels out when uniform
-
-    #     # # Mandy turns off meso for now
-    #     # f_surface_based_meso = f_surface_based_micro
-    #     f_surface_based_meso = f_diffuse
-
-    #     f_overall = f_surface_based_meso
-    #     f_overall *= dr.abs(cos_theta_o)
-    #     # multiplied by the cosine foreshortening factor, as in Mitsuba's documentation:
-    #     # https://mitsuba.readthedocs.io/en/latest/src/api_reference.html#mitsuba.BSDF.sample
-
-    #     perturb_weight = dr.select(self.perturb_specular, -dr.log(1.0 - self.pcg.next_float32()), 1.0)
-        
-    #     weight = (f_overall / bs.pdf) * perturb_weight
-    #     weight = dr.select(selected_dt, mi.Color3f(1.0, 1.0, 1.0), weight)
-    #     active = active & (dr.all(dr.isfinite(weight)))
-    #     # weight = weight & active 
-    #     weight = dr.select(active, weight, mi.Color3f(0.0, 0.0, 0.0))
-
-    #     return (bs, weight)
-
     def sample(self, ctx, si, sample1, sample2, active): 
         tiles = self.tiles
         tiled_uv = dr.select(self.feature_map_type == "cloth", (si.uv * tiles) - dr.trunc(si.uv * tiles), si.uv)
         color = mi.Color3f(self.texture.eval(tiled_uv)) # four different color parameters? (rd, rs, td, ts)
         cos_theta_i = mi.Frame3f.cos_theta(si.wi)
 
+        delta_transmission = mi.Vector1f(self.delta_transmission_map.eval(tiled_uv))    # TODO: think about the tiled_uv here
+        selected_dt = sample1 > delta_transmission.x
+
         bs = mi.BSDFSample3f()
-        reflect_or_transmit = sample1 < 0.5
+        reflect_or_transmit = sample1 < 0.5 #(TODO) probably good to decorrelate this with delta transmission check
 
         f_diffuse = color / math.pi        
         
@@ -369,9 +213,7 @@ class SurfaceBased (mi.BSDF) :
         normal = normal / dr.norm(normal)
         tangent= tangent / dr.norm(tangent)
         tangent = tangent - dr.sum(tangent * normal) * normal
-
-        delta_transmission = mi.Vector1f(self.delta_transmission_map.eval(tiled_uv))    # TODO: think about the tiled_uv here
-        selected_dt = sample1 > delta_transmission.x
+        
 
         S = dr.select(self.surface_or_fiber, S_surf, S_fibr)
         S = rotate_s_mat(S, normal, tangent)
@@ -424,7 +266,7 @@ class SurfaceBased (mi.BSDF) :
         active = active & dr.neq(D, 0.0) & dr.neq(dr.dot(bs.wo, h), 0.0)
         active = active & dr.neq(cos_theta_i * cos_theta_o_diffuse, 0.0) & (pdf > 0.0)
         
-        weight = dr.select(selected_dt, 1.0/pdf, f_surface_based_meso * dr.abs(cos_theta_o_diffuse) / pdf)
+        weight = dr.select(selected_dt, 1.0, f_surface_based_meso * dr.abs(cos_theta_o_diffuse) / pdf)
         weight = dr.select(active, weight, mi.Color3f(0.0,0.0,0.0))      
         pdf = dr.select(active, pdf, 0.0)
         bs.pdf = pdf
